a212890482deb2d54c1e9208d88318c66525e1ad
0x5448594f <derive.theo@proton.me>
2026-05-20 09:41:30 +0200

    Fix: Sphere Position

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default async function Home() {
           container={{
             top: "unset",
             right: "unset",
-            bottom: -500,
+            bottom: -450,
             left: 0,
             width: 800,
             height: 800
